991ec94e8f165197ba4bba557e234419f478cd4e
sun33sun <1255317579@qq.com>
2023-05-08 17:04:43 +0800

    Init HomeVisit

---
+++ Assets/Scripts/UI/CommunicationStepUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HomeVisit_URP.UI
+{
+	public class CommunicationStepUI : MonoBehaviour
+	{
+		public Image img;
+		[Header("«–ªª”√Õº∆¨")]
+		public Sprite spriteProgressBar;
+		public Sprite spriteC;
+		public Sprite spriteD;
+		public Sprite spriteH;
+	}
+}
+
