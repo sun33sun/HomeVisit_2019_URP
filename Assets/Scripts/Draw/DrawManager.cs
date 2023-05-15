eb33c0e318cd0e5b28a87854cc13b64d6c19549f
sun33sun <1255317579@qq.com>
2023-05-15 11:36:57 +0800

    1、UI图片替换、尺寸变更。 2、ScrollRect 停止水平滑动

--- Assets/Scripts/Draw/DrawManager.cs
+++ Assets/Scripts/Draw/DrawManager.cs
@@ -4,19 +4,49 @@ using UnityEngine.UI;
 
 namespace HomeVisit.Draw
 {
-    public class DrawManager : MonoSingleton<DrawManager>
+    public class DrawManager : MaskableGraphic
     {
-        [SerializeField] Button btnEraser;
-        [SerializeField] Button btnPaint;
-        [SerializeField] Button btnClear;
-        [SerializeField] Texture drawTex;
-        [Header("Runtime Editor")]
-        [SerializeField] Color color = Color.red;
-        [SerializeField] [Range(0.5f, 2f)] float width;
+        static DrawManager self;
 
-        void Start()
+        DrawManager()
         {
+            self = this;
+        }
 
+        static public void redraw()
+        {
+            print("redraw");
+            self.SetAllDirty();
+        }
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+            UIVertex vertex = UIVertex.simpleVert;
+            vertex.color = Color.red;
+
+            // draw a diagonal red line from bottom left to top right
+
+            // first triangle
+            vertex.position = new Vector2(0, 0); // lower left position 
+            vh.AddVert(vertex);
+            vertex.position = new Vector2(canvas.pixelRect.width + 2, canvas.pixelRect.height);
+            vh.AddVert(vertex);
+            vertex.position = new Vector2(canvas.pixelRect.width - 2, canvas.pixelRect.height);
+            vh.AddVert(vertex);
+
+            // second triangle
+            vertex.position = new Vector2(2, 0);
+            vh.AddVert(vertex);
+            vertex.position = new Vector2(-2, 0);
+            vh.AddVert(vertex);
+            vertex.position = new Vector2(canvas.pixelRect.width, canvas.pixelRect.height);
+            // position it on the upper right.
+            vh.AddVert(vertex);
+
+            // draw two elongated triangles to form a straight line
+            vh.AddTriangle(0, 1, 2); // only triangles can be drawn
+            vh.AddTriangle(3, 4, 5); // only triangles can be drawn
         }
     }
 }
